14f06c18e07026d028f6bbd16b7a2e3e4fb1dd7f
Kunal Das <kunal.dusk@gmail.com>
2023-11-11 13:47:00 +0530

    feat: integrate React Query and toast provider

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,9 @@ export default tseslint.config(
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
+      ...eslintPluginReactRefresh.configs.vite.rules,
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off'
     }
   },
   eslintConfigPrettier
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,31 @@
+import { ToastProvider } from './providers/toast-provider'
+import { QueryProvider } from './providers/query-provider'
+import { useToast } from './providers/toast-context'
+
+function ToastExample(): React.JSX.Element {
+  const { showToast } = useToast()
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-500">Hello World</h1>
+
+      <div className="flex gap-2">
+        <button onClick={() => showToast('Hello', 'World', 'success')}>Show Toast</button>
+        <button onClick={() => showToast('Hello', 'World', 'error')}>Show Error Toast</button>
+        <button onClick={() => showToast('Hello', 'World', 'neutral')}>Show Neutral Toast</button>
+      </div>
+    </>
+  )
+}
+
 function App(): React.JSX.Element {
-  return <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
+  return (
+    <QueryProvider>
+      <ToastProvider>
+        <ToastExample />
+      </ToastProvider>
+    </QueryProvider>
+  )
 }
 
 export default App
