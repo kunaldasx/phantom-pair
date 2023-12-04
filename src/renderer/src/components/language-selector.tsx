7e3666e449cb8d56e1901060383236d9274fe2e8
Kunal Das <kunal.dusk@gmail.com>
2023-12-04 21:18:00 +0530

    fix: correct language selector visibility

--- src/renderer/src/components/language-selector.tsx
+++ src/renderer/src/components/language-selector.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from 'react'
+import React from 'react'
+import { languageOptions } from '@renderer/lib/languages'
 
 interface LanguageSelectorProps {
   currentLanguage: string
@@ -9,23 +10,13 @@ export const LanguageSelector: React.FC<LanguageSelectorProps> = ({
   currentLanguage,
   setLanguage
 }) => {
-  const selectRef = useRef<HTMLSelectElement>(null)
-
-  useEffect(() => {
-    if (selectRef.current) {
-      selectRef.current.disabled = false
-    }
-  }, [])
-
-  const handleLanguageChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleRadioChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const newLanguage = e.target.value
 
     try {
       await window.electronAPI.updateConfig({ language: newLanguage })
-
       window.__LANGUAGE__ = newLanguage
       setLanguage(newLanguage)
-
       console.log('Language updated to:', newLanguage)
     } catch (error) {
       console.error('Error updating language:', error)
@@ -33,65 +24,28 @@ export const LanguageSelector: React.FC<LanguageSelectorProps> = ({
   }
 
   return (
-    <div className="mb-3 px-2 space-y-1">
-      <div className="flex items-center justify-between text-[13px] font-medium text-white/90">
-        <span>Language</span>
-        <select
-          ref={selectRef}
-          value={currentLanguage}
-          onChange={handleLanguageChange}
-          className="bg-black/80 text-white/90 rounded px-2 py-1 text-sm outline-none border border-white/10 focus:border-white/20"
-          style={{
-            opacity: 1,
-            pointerEvents: 'auto'
-          }}
-        >
-          <option value="python" className="bg-black text-white">
-            Python
-          </option>
-          <option value="javascript" className="bg-black text-white">
-            JavaScript
-          </option>
-          <option value="typescript" className="bg-black text-white">
-            TypeScript
-          </option>
-          <option value="java" className="bg-black text-white">
-            Java
-          </option>
-          <option value="csharp" className="bg-black text-white">
-            C#
-          </option>
-          <option value="go" className="bg-black text-white">
-            Go
-          </option>
-          <option value="ruby" className="bg-black text-white">
-            Ruby
-          </option>
-          <option value="php" className="bg-black text-white">
-            PHP
-          </option>
-          <option value="kotlin" className="bg-black text-white">
-            Kotlin
-          </option>
-          <option value="swift" className="bg-black text-white">
-            Swift
-          </option>
-          <option value="rust" className="bg-black text-white">
-            Rust
-          </option>
-          <option value="scala" className="bg-black text-white">
-            Scala
-          </option>
-          <option value="cpp" className="bg-black text-white">
-            C++
-          </option>
-          <option value="sql" className="bg-black text-white">
-            SQL
-          </option>
-          <option value="r" className="bg-black text-white">
-            R
-          </option>
-        </select>
+    <div className="mb-3 px-2 space-y-2">
+      <span className="block text-[13px] font-medium text-white/90 mb-1">Language</span>
+      <div className="space-y-1 max-h-40 overflow-y-auto pr-2">
+        {languageOptions.map((option) => (
+          <div key={option.value} className="flex items-center">
+            <input
+              type="radio"
+              id={`lang-${option.value}`}
+              name="language-selector"
+              value={option.value}
+              checked={currentLanguage === option.value}
+              onChange={handleRadioChange}
+              className="mr-2 h-3 w-3 cursor-pointer accent-blue-500"
+            />
+            <label
+              htmlFor={`lang-${option.value}`}
+              className="text-sm text-white/80 cursor-pointer hover:text-white"
+            >
+              {option.label}
+            </label>
+          </div>
+        ))}
       </div>
     </div>
   )
